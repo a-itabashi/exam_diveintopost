86f1ef90ec59e5b22a8eab0a1eb0591af9860bfe
Aguru itabashi <itabashi.a@gmail.com>
2019-04-15 13:53:03 +0900

    アジェンダの削除,ボタンの表示切り替え完了

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,6 +1,9 @@
 class AgendasController < ApplicationController
   # before_action :set_agenda, only: %i[show edit update destroy]
 
+  # Agendaを削除できるのは、user_id(Agendaの作者)もしくはteam_id(チームの作者)
+  before_action :allowed_only_user_or_owner, only: %i[destroy]
+
   def index
     @agendas = Agenda.all
   end
@@ -40,5 +43,13 @@ class AgendasController < ApplicationController
 
   def agenda_params
     params.fetch(:agenda, {}).permit %i[title description]
+  end
+
+  def allowed_only_user_or_owner
+    @agenda = Agenda.find(params[:id])
+    @team = Team.find_by(owner_id: current_user.id)
+    unless current_user.id == @agenda.user_id || @team.owner_id == @agenda.team_id
+      redirect_to dashboard_url, notice: '権限がありません'
+    end
   end
 end
