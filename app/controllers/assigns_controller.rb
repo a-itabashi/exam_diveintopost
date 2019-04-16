705230e348f08d18aa6f171629ab2cf11ca8afd5
Aguru itabashi <itabashi.a@gmail.com>
2019-04-16 11:52:09 +0900

    teamコントローラー実装完了

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -15,7 +15,6 @@ class AssignsController < ApplicationController
   def destroy
     assign = Assign.find(params[:id])
     destroy_message = assign_destroy(assign, assign.user)
-
     redirect_to team_url(params[:team_id]), notice: destroy_message
   end
 
@@ -30,6 +29,12 @@ class AssignsController < ApplicationController
       'リーダーは削除できません。'
     elsif Assign.where(user_id: assigned_user.id).count == 1
       'このユーザーはこのチームにしか所属していないため、削除できません。'
+    # 追加
+    elsif current_user.id == assign.team.owner
+      '管理者権限がないため削除できない'
+    elsif current_user.id == assign_user
+    # 追加
+      '本人ではないため削除できない'
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       'メンバーを削除しました。'
