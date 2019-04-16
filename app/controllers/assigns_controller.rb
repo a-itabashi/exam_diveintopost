9d6efad10a0dd23937e6713715d918d586714f42
Aguru itabashi <itabashi.a@gmail.com>
2019-04-16 12:12:51 +0900

    assignコントローラー実装完了

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -25,22 +25,24 @@ class AssignsController < ApplicationController
   end
 
   def assign_destroy(assign, assigned_user)
-    if assigned_user == assign.team.owner
+    if current_user.id == assigned_user.id
+      assign.destroy
+      set_next_team(assign, assigned_user)
+      'メンバーを削除しました。'
+    elsif assigned_user == assign.team.owner
       'リーダーは削除できません。'
     elsif Assign.where(user_id: assigned_user.id).count == 1
       'このユーザーはこのチームにしか所属していないため、削除できません。'
-    # 追加
-    elsif current_user.id == assign.team.owner
+    elsif current_user.id != assign.team.owner
       '管理者権限がないため削除できない'
-    elsif current_user.id == assign_user
-    # 追加
+    elsif current_user.id != assigned_user.id
       '本人ではないため削除できない'
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       'メンバーを削除しました。'
     else
       'なんらかの原因で、削除できませんでした。'
-    end    
+    end   
   end  
   
   def email_reliable?(address)
